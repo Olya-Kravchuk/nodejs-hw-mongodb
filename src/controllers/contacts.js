708b88805c2cf1646232f3d78feb53f05ede355d
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2024-06-10 17:18:01 +0200

    object

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -119,6 +119,6 @@ export const patchContactController = async (req, res, next) => {
   res.json({
     status: 200,
     message: `Successfully patched a contact!`,
-    data: result,
+    data: result.contact,
   });
 };
